d96ae57e920f3ef6d1d2fb7b9ca8ce199a501f59
qkxwsm <davidhu598@gmail.com>
2019-12-31 16:44:37 -0800

    Create orderedset.cpp

---
+++ orderedset.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
