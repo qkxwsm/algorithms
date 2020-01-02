d0c62a8b841af4de58e1eac455f9ac8faaa82c4e
qkxwsm <davidhu598@gmail.com>
2020-01-02 09:15:37 -0800

    Create rope.cpp

---
+++ rope.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/rope>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+//for more info: https://web.archive.org/web/20121225183151/http://www.sgi.com/tech/stl/Rope.html
