c5771697645bcfb4134cbbf51f4fcd13574c680e
qkxwsm <davidhu598@gmail.com>
2020-02-20 11:32:59 -0800

    Update gauss.cpp

--- general/gauss.cpp
+++ general/gauss.cpp
@@ -1,3 +1,40 @@
+//solve system of linear equations
+
+FOR(i, 0, N)
+{
+	if (mat[i][i] < EPS && mat[i][i] > -EPS)
+	{
+		int idx = -1;
+		FOR(j, i + 1, R)
+		{
+			if (mat[j][i] > 0.1 - EPS || mat[j][i] < -0.1 + EPS)
+			{
+				idx = j; break;
+			}
+		}
+		FOR(k, 0, N)
+		{
+			mat[i][k] += mat[idx][k];
+		}
+		val[i] += val[idx];
+	}
+	val[i] /= mat[i][i];
+	FORD(j, N, i)
+	{
+		mat[i][j] /= mat[i][i];
+	}
+	FOR(j, 0, R)
+	{
+		if (j == i) continue;
+		ld co = mat[j][i];
+		val[j] -= co * val[i];
+		FOR(k, 0, N)
+		{
+			mat[j][k] -= co * mat[i][k];
+		}
+	}
+}
+
 mat getinv(mat m)
 {
     mat res;
