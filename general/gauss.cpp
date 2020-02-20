42fed0372aa2f50899364f661b4c797b2ea7dfe9
qkxwsm <davidhu598@gmail.com>
2020-02-20 11:30:39 -0800

    Update gauss.cpp

--- general/gauss.cpp
+++ general/gauss.cpp
@@ -1,34 +1,40 @@
-        FOR(i, 0, N)
+mat getinv(mat m)
+{
+    mat res;
+    FOR(i, 0, SZ(res))
+    {
+        FOR(j, 0, SZ(res))
         {
-            if (abs(mat[i][i]) < EPS)
+            res[i][j] = 0;
+        }
+        res[i][i] = 1;
+    }
+    FOR(i, 0, SZ(m))
+    {
+        if (m[i][i] == 0)
+        {
+            FOR(j, i + 1, SZ(m))
             {
-                int idx = -1;
-                FOR(j, i + 1, R)
+                if (m[j][i] != 0)
                 {
-                    if (abs(mat[j][i]) > EPS)
-                    {
-                        idx = j; break;
-                    }
-                }
-                FOR(k, 0, N)
-                {
-                    mat[i][k] += mat[idx][k];
-                }
-                val[i] += val[idx];
-            }
-            val[i] /= mat[i][i];
-            FORD(j, N, i)
-            {
-                mat[i][j] /= mat[i][i];
-            }
-            FOR(j, 0, R)
-            {
-                if (j == i) continue;
-                ld co = mat[j][i];
-                val[j] -= co * val[i];
-                FOR(k, 0, N)
-                {
-                    mat[j][k] -= co * mat[i][k];
+                    m[i] = add(m[i], m[j]);
+                    res[i] = add(res[i], res[j]);
+                    break;
                 }
             }
         }
+        print(m); print(res);
+        int v = dvd(1, m[i][i]);
+        m[i] = mul(m[i], v);
+        res[i] = mul(res[i], v);
+        print(m); print(res);
+        FOR(j, 0, SZ(m))
+        {
+            if (i == j) continue;
+            int x = m[j][i]; x = sub(0, x);
+            m[j] = add(m[j], mul(m[i], x));
+            res[j] = add(res[j], mul(res[i], x));
+        }
+    }
+    return res;
+}
