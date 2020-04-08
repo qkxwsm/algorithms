a170100ffc662d42c044cd26e7298656861d2267
qkxwsm <davidhu598@gmail.com>
2020-04-07 21:16:08 -0700

    Update angularsort.cpp

--- general/angularsort.cpp
+++ general/angularsort.cpp
@@ -15,3 +15,33 @@ bool cmp(pll a, pll b)
     }
     return a.se * b.fi < a.fi * b.se;
 }
+
+bool between(pll a, pll b, pll c)
+{
+    if (cmp(a, c))
+    {
+        return cmp(a, b) && cmp(b, c);
+    }
+    else
+    {
+        return !(cmp(c, b) && cmp(b, a));
+    }
+}
+ll tris(vpl v) //how many triangles contain the origin?
+{
+    int n = SZ(pts);
+    ll res = 0;
+    sort(ALL(pts), cmp);
+    int iter = 0;
+    FOR(i, 0, n)
+    {
+        pll opp = {-pts[i].fi, -pts[i].se};
+        while(iter + 1 < i + n && between(pts[i], pts[(iter + 1) % n], opp)) iter++;
+        ll num = iter - i;
+        res += c2(num);
+        res += c2(n - 1 - num);
+    }
+    res /= 2;
+    res = c3(n) - res;
+    return res;
+}
