232ff569ed282f1089dadc8479b4a9f31e3cd8d4
qkxwsm <davidhu598@gmail.com>
2020-05-08 10:48:25 -0700

    Update cht1.cpp

--- general/cht1.cpp
+++ general/cht1.cpp
@@ -4,7 +4,7 @@
     FOR(i, 1, N + 1)
     {
         while(iter + 1 < SZ(opt) && dp[opt[iter + 1]] + cost(opt[iter + 1], i) <= dp[opt[iter]] + cost(opt[iter], i)) iter++;
-        dp[i] = dp[opt[iter]] + cost(opt[iter], i) + B[i] * (S - A[i]);
+        dp[i] = dp[opt[iter]] + cost(opt[iter], i);
         while(SZ(opt) >= 2 && intersect(opt[SZ(opt) - 2], opt.back()) > intersect(opt.back(), i)) opt.pop_back();
         opt.PB(i);
         ckmin(iter, SZ(opt) - 1);
