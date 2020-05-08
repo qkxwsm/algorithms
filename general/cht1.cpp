cdaeeaa7da0a5e2e3077dc8674595bd4fd8384d9
qkxwsm <davidhu598@gmail.com>
2020-05-08 10:47:45 -0700

    Create cht1.cpp

---
+++ general/cht1.cpp
@@ -0,0 +1,11 @@
+
+    vi opt; opt.PB(0); int iter = 0;
+    dp[0] = B[0] * (S - A[0]);
+    FOR(i, 1, N + 1)
+    {
+        while(iter + 1 < SZ(opt) && dp[opt[iter + 1]] + cost(opt[iter + 1], i) <= dp[opt[iter]] + cost(opt[iter], i)) iter++;
+        dp[i] = dp[opt[iter]] + cost(opt[iter], i) + B[i] * (S - A[i]);
+        while(SZ(opt) >= 2 && intersect(opt[SZ(opt) - 2], opt.back()) > intersect(opt.back(), i)) opt.pop_back();
+        opt.PB(i);
+        ckmin(iter, SZ(opt) - 1);
+    }
