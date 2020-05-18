9fb2f03e4dc006acc8033b33fcaf0af1a697d67b
qkxwsm <davidhu598@gmail.com>
2020-05-18 10:17:08 -0700

    Update fenwick.cpp

--- general/fenwick.cpp
+++ general/fenwick.cpp
@@ -1,3 +1,19 @@
+void update(int idx, ll v)
+{
+    for (int e = idx + 1; e < MAXN; e += e & (-e))
+    {
+        fen[e] += v;
+    }
+}
+ll query(int idx)
+{
+    ll res = 0;
+    for (int e = idx + 1; e; e -= e & (-e))
+    {
+        res += fen[e];
+    }
+    return res;
+}
 int kth(ll x) //acts like upper_bound
 {
     //assert((res == 0 || query(res - 1) <= x) && query(res) > x);
